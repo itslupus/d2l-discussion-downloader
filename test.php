113c6d9244726923db986eb73269b7d6cb1d4439
Lucas <contact@itslucas.win>
2019-11-29 23:35:02 -0600

    remove redundant (for now) headers

--- test.php
+++ test.php
@@ -51,64 +51,6 @@
         curl_setopt($curl, CURLOPT_URL, 'https://universityofmanitoba.desire2learn.com/d2l/le/manageCourses/search/6606/GridReloadPartial');
         curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, "gridPartialInfo\$_type=D2L.LP.Web.UI.Desktop.Controls.GridPartialArgs&gridPartialInfo\$SortingInfo\$SortField=OrgUnitName&gridPartialInfo\$SortingInfo\$SortDirection=0&gridPartialInfo\$NumericPagingInfo\$PageNumber=1&gridPartialInfo\$NumericPagingInfo\$PageSize=10&searchTerm=&status=-1&toStartDate\$Year=$date[0]&toStartDate\$Month=$date[1]&toStartDate\$Day=$date[2]&toStartDate\$Hour=9&toStartDate\$Minute=0&toStartDate\$Second=0&fromStartDate\$Year=$date[0]&fromStartDate\$Month=$date[1]&fromStartDate\$Day=$date[2]&fromStartDate\$Hour=9&fromStartDate\$Minute=0&fromStartDate\$Second=0&toEndDate\$Year=$date[0]&toEndDate\$Month=$date[1]&toEndDate\$Day=$date[3]&toEndDate\$Hour=9&toEndDate\$Minute=0&toEndDate\$Second=0&fromEndDate\$Year=$date[0]&fromEndDate\$Month=$date[1]&fromEndDate\$Day=$date[2]&fromEndDate\$Hour=9&fromEndDate\$Minute=0&fromEndDate\$Second=0&hasToStartDate=False&hasFromStartDate=False&hasToEndDate=False&hasFromEndDate=true&filtersFormId\$Value=$id&_d2l_prc\$headingLevel=2&_d2l_prc\$scope&_d2l_prc\$childScopeCounters=filtersData:0;FromStartDate:0;ToStartDate:0;FromEndDate:0;ToEndDate:0&_d2l_prc\$hasActiveForm=false&filtersData\$semesterId=All&filtersData\$departmentId=All&isXhr=true&requestId=4&d2l_referrer=$token");
-        // curl_setopt($curl, CURLOPT_POSTFIELDS, '
-        //     gridPartialInfo$_type=D2L.LP.Web.UI.Desktop.Controls.GridPartialArgs' . '
-        //     &gridPartialInfo$SortingInfo$SortField=OrgUnitName' . '
-        //     &gridPartialInfo$SortingInfo$SortDirection=0' . '
-        //     &gridPartialInfo$NumericPagingInfo$PageNumber=1' . '
-        //     &gridPartialInfo$NumericPagingInfo$PageSize=10' . '
-        //     &searchTerm=' . '
-        //     &status=-1' . '
-        //     &toStartDate$Year=2019' . '
-        //     &toStartDate$Month=11' . '
-        //     &toStartDate$Day=29' . '
-        //     &toStartDate$Hour=21' . '
-        //     &toStartDate$Minute=0' . '
-        //     &toStartDate$Second=0' . '
-        //     &fromStartDate$Year=2019' . '
-        //     &fromStartDate$Month=11' . '
-        //     &fromStartDate$Day=29' . '
-        //     &fromStartDate$Hour=21' . '
-        //     &fromStartDate$Minute=0' . '
-        //     &fromStartDate$Second=0' . '
-        //     &toEndDate$Year=2019' . '
-        //     &toEndDate$Month=11' . '
-        //     &toEndDate$Day=29' . '
-        //     &toEndDate$Hour=21' . '
-        //     &toEndDate$Minute=0' . '
-        //     &toEndDate$Second=0' . '
-        //     &fromEndDate$Year=2019' . '
-        //     &fromEndDate$Month=11' . '
-        //     &fromEndDate$Day=29' . '
-        //     &fromEndDate$Hour=21' . '
-        //     &fromEndDate$Minute=0' . '
-        //     &fromEndDate$Second=0' . '
-        //     &hasToStartDate=False' . '
-        //     &hasFromStartDate=False' . '
-        //     &hasToEndDate=False' . '
-        //     &hasFromEndDate=true' . '
-        //     &filtersFormId$Value=' . $id . '
-        //     &_d2l_prc$headingLevel=2' . '
-        //     &_d2l_prc$scope=' . '
-        //     &_d2l_prc$childScopeCounters=filtersData:0;FromStartDate:0;ToStartDate:0;FromEndDate:0;ToEndDate:0' . '
-        //     &_d2l_prc$hasActiveForm=false&filtersData$semesterId=All' . '
-        //     &filtersData$departmentId=All' . '
-        //     &isXhr=true&requestId=4' . '
-        //     &d2l_referrer=' . $token
-        // );
-        // curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-        //     'Accept' => '*/*',
-        //     'Accept-Encoding' => 'gzip, deflate, br',
-        //     'Accept-Language' => 'en-CA,en-US;q=0.7,en;q=0.3',
-        //     'Cache-Control' => 'no-cache',
-        //     'Connection' => 'keep-alive',
-        //     'Content-type' => 'application/x-www-form-urlencoded',
-        //     'Host' => 'universityofmanitoba.desire2learn.com',
-        //     'Origin' => 'https://universityofmanitoba.desire2learn.com',
-        //     'Pragma' => 'no-cache',
-        //     'Referer' => 'https://universityofmanitoba.desire2learn.com/d2l/le/manageCourses/search/6606',
-        //     'TE' => "Trailers"
-        // ));
         $result = curl_exec($curl);
         $courses = fopen(__DIR__ . '/test-courses.json', 'w+');
         fwrite($courses, $result);
