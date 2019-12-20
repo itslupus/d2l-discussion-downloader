45a42d37198323db92c52b964a1c78a77b7fe4bf
Lucas <contact@itslucas.win>
2019-12-19 19:23:04 -0600

    renamed constants

---
+++ globals.php
@@ -0,0 +1,11 @@
+<?php
+    define('GLOBAL_ROOT_PATH', __DIR__);
+
+    define('GLOBAL_TEMPLATE_PATH',     '/templates');
+    define('GLOBAL_WEBMASTER_EMAIL',   'contact[at]itslucas[dot]win');
+
+    define('GLOBAL_SQL_DATABASE',      '');
+    define('GLOBAL_SQL_IP',            '');
+    define('GLOBAL_SQL_USERNAME',      '');
+    define('GLOBAL_SQL_PASSWORD',      '');
+?>
