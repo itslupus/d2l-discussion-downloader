f2b95c7fe13dbd2fa45a6b62e571f08b296f5543
Lucas <contact@itslucas.win>
2020-01-05 20:38:30 -0600

    updated home page

--- templates/default/index.php
+++ templates/default/index.php
@@ -7,31 +7,49 @@
         <script src = 'https://code.iconify.design/1/1.0.3/iconify.min.js'></script>
     </head>
     <body>
-        <div class = 'row welcome'>
-            <div><span class = 'iconify icon:mdi:worker icon-inline:false icon-width:10rem icon-height:10rem'></span></div>
+        <header>
             <div>
-                <h1>Under Construction</h1>
-                <p>This site is currently in the makings!</p>
+                <a href = '/'>home</a>
             </div>
-        </div>
-        <div class = 'row'>
-            <div class = 'centered'>
-                <h2>styles</h2>
-                <p>check out some dank styles we have</p>
+        </header>
+        <main>
+            <div class = 'row welcome'>
+                <div><span class = 'iconify icon:mdi:worker icon-inline:false icon-width:10rem icon-height:10rem'></span></div>
+                <div>
+                    <h1>Under Construction</h1>
+                    <p>This site is currently in the makings!</p>
+                    <button class = 'get-started-button'><a href = '/login.php'>get started</a></button>
+                    </div>
+                </div>
             </div>
-            <div class = 'row-display'>
-                <div><span class = 'iconify icon:mdi:file icon-inline:false icon-width:10rem icon-height:10rem'></span></div>
-                <div><span class = 'iconify icon:mdi:file icon-inline:false icon-width:10rem icon-height:10rem'></span></div>
-                <div><span class = 'iconify icon:mdi:file icon-inline:false icon-width:10rem icon-height:10rem'></span></div>
+            <div class = 'row'>
+                <div class = 'centered'>
+                    <h2>styles</h2>
+                    <p>check out some dank styles we have</p>
+                </div>
+                <div class = 'row-display'>
+                    <div><span class = 'iconify icon:mdi:file icon-inline:false icon-width:10rem icon-height:10rem'></span></div>
+                    <div><span class = 'iconify icon:mdi:file icon-inline:false icon-width:10rem icon-height:10rem'></span></div>
+                    <div><span class = 'iconify icon:mdi:file icon-inline:false icon-width:10rem icon-height:10rem'></span></div>
+                </div>
             </div>
-        </div>
+            <div class = 'row'>
+                <div class = 'centered'>
+                    <h2>some other row</h2>
+                    <p>something else</p>
+                </div>
+                <div class = 'row-display'>
+                    <div><span class = 'iconify icon:mdi:file icon-inline:false icon-width:10rem icon-height:10rem'></span></div>
+                </div>
+            </div>
+        </main>
+        <footer>
+            <div>
+                element one
+            </div>
+            <div>
+                element two
+            </div>
+        </footer>
     </body>
-    <footer>
-        <div>
-            element one
-        </div>
-        <div>
-            element two
-        </div>
-    </footer>
 </html>
